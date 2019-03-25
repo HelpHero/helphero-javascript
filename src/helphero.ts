77aeb287afec1d85fba54c7e5c55fcaa4762f175
Neil Jagtiani <nkjchamp25@gmail.com>
2019-03-25 16:35:25 -0400

    just a freaking test

--- src/helphero.ts
+++ src/helphero.ts
@@ -71,7 +71,7 @@ const methods = [
 let initializedAppId: string;
 let instance: HelpHero & AsyncHelpHero & { [method: string]: Function };
 
-export default function initHelpHero(appId: string, embedPrefix: string): HelpHero {
+export default function initHelpHero(appId: string): HelpHero {
   if (typeof appId !== "string" || appId === "") {
     throw new Error(`Invalid HelpHero App ID: ${appId}`);
   }
@@ -94,7 +94,7 @@ export default function initHelpHero(appId: string, embedPrefix: string): HelpHe
 
   // add script to page
   var script = document.createElement("script");
-  script.src = `${embedPrefix || ""}//app.helphero.co/embed/${appId}`;
+  script.src = `https://app.helphero.co/embed/${appId}`;
   script.async = true;
   document.body.appendChild(script);
 
